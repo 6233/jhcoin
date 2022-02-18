0842ae6cc5d957925597ad63fd2e0367bfa0827c
jhleeO <47955896+jhleeO@users.noreply.github.com>
2022-02-18 22:13:15 +0900

    refactoring

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/6233/jhcoin/blockchain"
+import (
+	"fmt"
+
+	"github.com/6233/jhcoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlock() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hata: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }
