ce7e46c60285435dfee223e508e662b92d1808b7
jhleeO <47955896+jhleeO@users.noreply.github.com>
2022-03-12 21:21:32 +0900

    Flag

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"github.com/6233/jhcoin/cli"
 )
 
-func usage() {
-	fmt.Printf("Welcome to jhcoin\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer:	Start the HTML Explorer\n")
-	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
-	os.Exit(0)
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
+	cli.Start()
 }
