c2f62a341ac34787f2a38dcf9b8783dba384a5c0
jhleeO <47955896+jhleeO@users.noreply.github.com>
2022-03-28 19:57:59 +0900

    Signing Messages

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/6233/jhcoin/db"
-	"github.com/6233/jhcoin/cli"
+	"github.com/6233/jhcoin/wallet"
 )
 
 func main() {
-	defer db.Close()
-	cli.Start()
+	wallet.Start()
 }
