23c72019ee2e4fe65dbf017c0f42c1235508b342
jhleeO <47955896+jhleeO@users.noreply.github.com>
2022-03-28 20:51:00 +0900

    Restoring

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,12 +1,9 @@
 package wallet
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
 	"crypto/x509"
 	"encoding/hex"
-	"fmt"
+	"math/big"
 
 	"github.com/6233/jhcoin/utils"
 )
@@ -17,29 +14,21 @@ const (
 	privateKey    string = "307702010104204eb9438b09d338be90fdc55d9391d4d5e10f620ca458818a3941e0bd400c2514a00a06082a8648ce3d030107a14403420004cf39c60bcf171dfdb10d9d51345558c522169e23f4c148c80658cac50303f0e7d410fe4990f2835793ab6fc5f53d81260346deb6dc38d2041f35d88c31f7fd9c"
 	hashedMessage string = "1c5863cd55b5a4413fd59f054af57ba3c75c0698b3851d70f99b8de2d5c7338f"
 )
-
 func Start() {
 
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-
+	privBytes, err := hex.DecodeString(privateKey)
 	utils.HandleErr(err)
 
-	keyAsBytes, err := x509.MarshalECPrivateKey(privateKey)
-
-	fmt.Printf("%x\n\n\n\n\n", keyAsBytes)
-
+	private, err := x509.ParseECPrivateKey(privBytes)
 	utils.HandleErr(err)
 
-	hashAsBytes, err := hex.DecodeString(hashedMessage)
+	sigBytes, err := hex.DecodeString(signature)
+	rBytes := sigBytes[:len(sigBytes)/2]
+	sBytes := sigBytes[len(sigBytes)/2:]
 
-	utils.HandleErr(err)
+	var bigR, bigS = big.Int{}, big.Int{}
 
-	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
-
-	signature := append(r.Bytes(), s.Bytes()...)
-
-	fmt.Printf("%x\n", signature)
-
-	utils.HandleErr(err)
+	bigR.SetBytes(rBytes)
+	bigS.SetBytes(sBytes)
 
 }
