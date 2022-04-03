0799d8d6d88c8b4a9890789dd220b16a671fbe8c
jhleeO <47955896+jhleeO@users.noreply.github.com>
2022-04-03 11:22:04 +0900

    Read Peer

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/6233/jhcoin/utils"
@@ -20,10 +21,14 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 	initPeer(conn, ip, openPort)
+	time.Sleep(20 * time.Second)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
 }
 func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
+	time.Sleep(10 * time.Second)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
 }
